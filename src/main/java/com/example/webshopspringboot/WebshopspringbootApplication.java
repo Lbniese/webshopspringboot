89513aedbdde32efa398b117752a6ba2047a4b48
Lbniese <Lbniese@users.noreply.github.com>
2020-09-18 11:19:03 +0200

    DataSource Bean

--- src/main/java/com/example/webshopspringboot/WebshopspringbootApplication.java
+++ src/main/java/com/example/webshopspringboot/WebshopspringbootApplication.java
@@ -1,10 +1,17 @@
 package com.example.webshopspringboot;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
+import java.sql.DriverManager;
 
 @SpringBootApplication(exclude = {
         DataSourceAutoConfiguration.class,
@@ -14,8 +21,25 @@ import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfigurat
 
 public class WebshopspringbootApplication {
 
+    @Autowired
+    Environment env;
+
     public static void main(String[] args) {
         SpringApplication.run(WebshopspringbootApplication.class, args);
+    }
+
+    @Bean(name ="dataSource")
+    public DataSource getDataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+
+        // Checker application.properties filen
+        dataSource.setDriverClassName(env.getProperty("spring.datasource.driver-class-name"));
+        dataSource.setUrl(env.getProperty("spring.datasource.url"));
+        dataSource.setUsername(env.getProperty("spring.datasource.username"));
+        dataSource.setPassword(env.getProperty("spring.datasource.password"));
+
+        System.out.println("## getDataSource: "+ dataSource);
+        return dataSource;
     }
 
 }
