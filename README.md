360bfa23d36d065bc1da9662ee208e0bcd8bb074
Lbniese <Lbniese@users.noreply.github.com>
2020-09-18 11:15:40 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# webshopspringboot
+ 
---
+++ src/test/java/com/example/webshopspringboot/WebshopspringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webshopspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebshopspringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
